57061ad7b95881ca57da0ec66903b1a0470affdd
Leandecks <leandrogridelli@gmail.com>
2024-02-15 21:00:06 +0100

    Constructor -> Class

--- library.js
+++ library.js
@@ -7,14 +7,13 @@ const library = [];
 
 // Book constructor
 
-function Book (title, author, pages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-  this.info = () => {
-    return `${this.title} by ${author}, ${pages} pages, ${isRead ? "already read" : "not read yet"}`
-  };
+class Book {
+  constructor(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
 }
 
 // Add book to library
